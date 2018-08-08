c544df7e7a51001d049707b506ac062b698c2741
henryxiao <fortbox1988@126.com>
2018-08-08 13:36:49 +0800

    solve
    最基本的思考

--- No38_报数/Solution.java
+++ No38_报数/Solution.java
@@ -38,5 +38,29 @@ public class Solution {
 		return nowStr;
 
 	}
+	
+	
+	  public String countAndSay1(int n) {
+	        String str = "1";
+	        if(n==1){
+	            return str;
+	        }
+	        for (int i = 1; i < n; i++) {
+	            int count = 1;
+	            StringBuilder sb = new StringBuilder();
+	            char lastOne = '1';
+	            for (int j = 1; j < str.length(); j++) {
+	                if(str.charAt(j) == str.charAt(j-1)){
+	                    count++;
+	                }else {
+	                    sb.append(count).append(lastOne = str.charAt(j-1));
+	                    count = 1;
+	                }
+	            }
+	            sb.append(count).append(str.charAt(str.length()-1));
+	            str = sb.toString();
+	        }
+	        return str;
+	    }
 
 }

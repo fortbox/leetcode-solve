7ae43004cb0703e044026d5c8f6d2a2bd3748b09
henryxiao <fortbox1988@126.com>
2018-08-10 19:21:21 +0800

    做的两道题
    相对简单

---
+++ src/No88_合并两个有序数组/Solution.java
@@ -0,0 +1,26 @@
+package No88_合并两个有序数组;
+
+import java.util.Arrays;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/**
+	 * nums1=numss1+nums2;对nums1排序
+	 * @param nums1
+	 * @param m
+	 * @param nums2
+	 * @param n
+	 */
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int i = 0; i < nums2.length; i++) {
+			nums1[m-1+i]=nums2[i];
+		}
+        Arrays.sort(nums1);
+    }
+
+}

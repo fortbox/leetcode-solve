077ff067a027440656ce5d74ef219885fba7df61
henryxiao <fortbox1988@126.com>
2018-08-17 10:11:07 +0800

    杨辉三角，2018年8月17日
    有了基本原来后，去实现。基本原理，才是根本，能降低程序的时间复杂度和空间复杂度

---
+++ src/No121_买卖股票的最佳时机/Solution.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package No121_买卖股票的最佳时机;
+
+/**
+ * @author jrxiaoweixiang 2018年8月16日
+ */
+public class Solution {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int[] prices=new int[]{7,1,5,3,6,4};
+		Solution solution=new Solution();
+		System.out.println(solution.maxProfit1(prices));
+
+	}
+
+	/**
+	 * 遍历两次来判断
+	 * @param prices
+	 * @return
+	 */
+	public int maxProfit(int[] prices) {
+		int max = 0;
+		for (int i = 0; i < prices.length; i++) {
+			for (int j = i + 1; j < prices.length; j++) {
+				int m = prices[j] - prices[i];
+				max=Math.max(m, max);
+			}
+		}
+		return max;
+	}
+	
+	/**
+	 * 第二种方法，判断后面的相邻两个相差大于0，才相加，要不然后移。
+	 * @param prices
+	 * @return
+	 */
+	public int maxProfit1(int[] prices) {
+		int maxProfit = 0;
+		int minIndex=0;
+		for (int i = 1; i < prices.length; i++) {
+			maxProfit=Math.max(maxProfit, prices[i]-prices[minIndex]);
+			minIndex=(prices[i]-prices[minIndex])>0?minIndex:i;
+		}
+		return maxProfit;
+	}
+	
+	
+
+}

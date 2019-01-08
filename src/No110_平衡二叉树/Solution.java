387cb9a955d4d4fe526f208cad52ad4a95c16bb7
henryxiao <henryxiao@outlook.com>
2019-01-08 20:07:20 +0800

    110,111
    leetcode

--- src/No110_平衡二叉树/Solution.java
+++ src/No110_平衡二叉树/Solution.java
@@ -3,36 +3,36 @@ package No110_平衡二叉树;
 public class Solution {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
 	public boolean isBalanced(TreeNode root) {
-		// 1，如果有左子树，没有右子树，则检查左子树有无子书，若有，则返回false；若无，则返回true；
-		// 2，继续递归检查左右子树。
+		// 1，找到左子树的深度
+		// 2，找到右子树的深度
+		// 2，比较左子树，右子树的深度只差，如果大于1，则返回false
 		if (root == null) {
 			return true;
 		}
 		TreeNode left = root.left;
 		TreeNode right = root.right;
-		int a=0,b=0;
-		while(true) {
-			
+		int leftDepth = findDepth(left);
+		int rightDepth = findDepth(right);
+		if (Math.abs(leftDepth - rightDepth) > 1) {
+			return false;
+		} else {
+			return isBalanced(left)&&isBalanced(right);
 		}
 
-//		if (left == null && right != null) {
-//			if (right.left != null || right.right != null) {
-//				return false;
-//			}
-//		}
-//		if (left != null && right == null) {
-//			if (left.left != null || left.right != null) {
-//				return false;
-//			}
-//		}
-//		if (left==null&&right==null) {
-//			return true;
-//		}
-//		return isBalanced(left)&&isBalanced(right);
+	}
+	
+	public int findDepth(TreeNode treeNode) {
+		int height=0;
+		if (treeNode==null) {
+			return 0;
+		}
+		TreeNode left = treeNode.left;
+		TreeNode right = treeNode.right;
+		height=1+Math.max(findDepth(left), findDepth(right));
+		return height;
 	}
 }
---
+++ src/No111_二叉树的最小深度/TreeNode.java
@@ -0,0 +1,17 @@
+package No111_二叉树的最小深度;
+
+public class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int val) {
+		this.val = val;
+	}
+
+	@Override
+	public String toString() {
+		return "TreeNode [val=" + val + ", left=" + left + ", right=" + right + "]";
+	}
+	
+}

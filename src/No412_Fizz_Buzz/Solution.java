aae174bcc84a67cae714ff51f50a34c065561e8f
fortbox <fortbox1988@126.com>
2019-05-22 16:55:23 +0800

    LeetCode 412 Java by Henry

---
+++ src/No412_Fizz_Buzz/Solution.java
@@ -0,0 +1,33 @@
+package No412_Fizz_Buzz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+	public static void main(String[] args) {
+
+		Solution solution = new Solution();
+		System.out.println(solution.fizzBuzz(15).toString());
+	}
+
+	public List<String> fizzBuzz(int n) {
+		String s1 = "Fizz";
+		String s2 = "Buzz";
+		List<String> list = new ArrayList<String>();
+		for (int i = 1; i <= n; i++) {
+			String s;
+			if (i%3==0&&i%5!=0) {
+				s=s1;
+			}else if(i%3!=0&&i%5==0) {
+				s=s2;
+			}else if(i%3==0&&i%5==0) {
+				s=s1+s2;
+			}else {
+				s=String.valueOf(i);
+			}
+			list.add(s);
+		}
+		return list;
+	}
+}

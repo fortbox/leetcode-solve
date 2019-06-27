83b9d49407235231903c09e8d27b3e63412acb71
henryxiao <fortbox1988@outlook.com>
2019-06-27 23:44:50 +0800

    LeetCode

---
+++ src/No475_供暖器/Solution.java
@@ -0,0 +1,14 @@
+package No475_供暖器;
+
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+    public int findRadius(int[] houses, int[] heaters){
+
+        Arrays.parallelSort(houses);
+
+    }
+}

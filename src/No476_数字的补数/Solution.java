10ed55661327aaee0380e4dbfb3ffc42c900ff0a
henryxiao <fortbox1988@outlook.com>
2019-06-29 00:41:19 +0800

    修改空格

---
+++ src/No476_数字的补数/Solution.java
@@ -0,0 +1,15 @@
+package No476_数字的补数;
+
+public class Solution {
+    public int findComplement(int num){
+        //1.先判断几位
+        String s=Integer.toBinaryString(num);
+        int n=s.length();
+        //2.再求和
+        for (int i = 0; i < n; i++) {
+
+
+
+        }
+    }
+}

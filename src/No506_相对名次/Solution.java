b95247831aa3b7e3cca8d606303aa51c1f7dbd58
henryxiao <fortbox1988@outlook.com>
2019-07-05 00:39:03 +0800

    建立了方法，还未实现

---
+++ src/No506_相对名次/Solution.java
@@ -0,0 +1,21 @@
+package No506_相对名次;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     *
+     * @param nums
+     * @return
+     */
+    public String [] findRelativeRanks(int[] nums){
+        String[] sa=new String[nums.length];
+
+
+
+
+    }
+}

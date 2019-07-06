f5c931816b14ba2afa6da3d10023a83ebc3ebd5b
henryxiao <fortbox1988@outlook.com>
2019-07-07 02:14:14 +0800

    LeetCode 520 Java by Henry Xiao

---
+++ src/No520_检测大写字母/Solution.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package No520_检测大写字母;
+
+/**
+ * @author Henry Xiao
+ * @date 2019年7月7日
+ */
+public class Solution {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public boolean detectCapitalUse(String word) {
+		if (word.length() == 1) {
+			return true;
+		}
+		char c = word.charAt(0);
+		word = word.substring(1);
+		if (c >= 'a' && c <= 'z') {
+			return word.equals(word.toLowerCase());
+		} else {
+			return (word.equals(word.toLowerCase()) || word.equals(word.toUpperCase()));
+		}
+	}
+}

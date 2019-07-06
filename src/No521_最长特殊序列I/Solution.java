f39e2046af24ebebdc2c0ce0e9d14f39f04aab3a
henryxiao <fortbox1988@outlook.com>
2019-07-07 02:42:45 +0800

    LeetCode 521 Java by Henry Xiao

---
+++ src/No521_最长特殊序列I/Solution.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package No521_最长特殊序列I;
+
+/**
+ * @author Henry Xiao
+ * @date 2019年7月7日
+ */
+public class Solution {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int findLUSlength(String a, String b) {
+		int m = a.length();
+		int n = b.length();
+		if (a.equals(b)) {
+			return -1;
+		}
+		return Math.max(m, n);
+	}
+
+}

3f759b3edaec6d4666212d8a10d88f5a2e9f9de2
henryxiao <fortbox1988@outlook.com>
2019-07-14 17:58:16 +0800

    LeetCode 665 Java by Henry Xiao

---
+++ src/No665_非递减数列/Solution.java
@@ -0,0 +1,19 @@
+package No665_非递减数列;
+
+class Solution {
+    public boolean checkPossibility(int[] nums) {
+    int remeber = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] > nums[i]) {
+                remeber++;
+                if (i > 1 && nums[i - 2] > nums[i]) {
+                    nums[i] = nums[i - 1];
+                }else {
+                    nums[i-1]=nums[i];
+                }
+            }
+            if (remeber > 1) return false;
+        }
+        return true;
+    }
+}

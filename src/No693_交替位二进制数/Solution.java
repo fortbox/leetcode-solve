7fab433eccac03a78f1f3d1043c48a8b2b0d1cf6
henryxiao <fortbox1988@outlook.com>
2019-07-17 23:59:12 +0800

    LeetCode 693 Java by Henry Xiao

---
+++ src/No693_交替位二进制数/Solution.java
@@ -0,0 +1,11 @@
+package No693_交替位二进制数;
+
+class Solution {
+    public boolean hasAlternatingBits(int n) {
+        String s = Integer.toBinaryString(n);
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i - 1) == s.charAt(i)) return false;
+        }
+        return true;
+    }
+}

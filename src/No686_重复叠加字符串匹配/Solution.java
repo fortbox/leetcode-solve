0b6f6cdbd154edeec2e3beaea60b78faaaace0e2
henryxiao <fortbox1988@outlook.com>
2019-07-17 21:02:33 +0800

    LeetCode 686 Java by Henry Xiao

---
+++ src/No686_重复叠加字符串匹配/Solution.java
@@ -0,0 +1,17 @@
+package No686_重复叠加字符串匹配;
+
+class Solution {
+    public int repeatedStringMatch(String A, String B) {
+        String temp = A;
+        int count = 1;
+        while (A.length() < B.length()) {
+            A += temp;
+            count++;
+        }
+        if (A.contains(B)) return count;
+        A += temp;
+        count++;
+        if (A.contains(B)) return count;
+        return -1;
+    }
+}

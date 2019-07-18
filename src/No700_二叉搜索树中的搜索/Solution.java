24f13e39fc4c32acfaecd3e7fb38850182b755c3
henryxiao <fortbox1988@outlook.com>
2019-07-18 23:57:49 +0800

    LeetCode 700 Java by Henry Xiao

---
+++ src/No700_二叉搜索树中的搜索/Solution.java
@@ -0,0 +1,30 @@
+package No700_二叉搜索树中的搜索;
+
+
+class Solution {
+    //还是要利用二叉搜索树的性质啊
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == val) return root;
+        TreeNode left = searchBST(root.left, val);
+        if (left != null) return left;
+        TreeNode right = searchBST(root.right, val);
+        if (right != null) return right;
+        return null;
+    }
+
+
+    //    Definition for a binary tree node.
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+}

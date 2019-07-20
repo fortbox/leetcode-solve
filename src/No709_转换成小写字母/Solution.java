993068f58bfa46bcd337a08c4fbab522a78cacdd
henryxiao <fortbox1988@outlook.com>
2019-07-20 20:45:52 +0800

    LeetCode 709 Java by Henry Xiao

---
+++ src/No709_转换成小写字母/Solution.java
@@ -0,0 +1,16 @@
+package No709_转换成小写字母;
+
+class Solution {
+    public String toLowerCase(String str) {
+        int k = 'A' - 'a';
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c >= 'A' && c <= 'Z') {
+                c = (char) (c - k);
+            }
+            builder.append(c);
+        }
+        return builder.toString();
+    }
+}

8eddc50776c61899a8eb5d8df314e1c649591132
henryxiao <fortbox1988@outlook.com>
2019-07-21 11:21:52 +0800

    LeetCode 724 Java by Henry Xiao

---
+++ src/No724_寻找数组的中心索引/Solution.java
@@ -0,0 +1,25 @@
+package No724_寻找数组的中心索引;
+
+class Solution {
+    public static void main(String[] args) {
+        int[] nums={10};
+        int result=pivotIndex(nums);
+        System.out.println("result = " + result);
+    }
+
+    public static int pivotIndex(int[] nums) {
+        //数组所有元素相加得到一个和
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        //从左开始遍历直到找到和的一半
+        int temp = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum -= nums[i];
+            if (temp == sum) return i;
+            temp += nums[i];
+        }
+        return -1;
+    }
+}

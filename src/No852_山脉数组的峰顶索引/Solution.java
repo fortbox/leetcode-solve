1b451d841961537a83b6b7602b13c5bbac34145c
henryxiao <fortbox1988@outlook.com>
2019-07-28 11:21:55 +0800

    LeetCode 852 Java by Henry Xiao

---
+++ src/No852_山脉数组的峰顶索引/Solution.java
@@ -0,0 +1,15 @@
+package No852_山脉数组的峰顶索引;
+
+class Solution {
+    public int peakIndexInMountainArray(int[] A) {
+        int maxi = 0;
+        int maxnum = Integer.MIN_VALUE;
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] > maxnum) {
+                maxi = i;
+                maxnum=A[i];
+            }
+        }
+        return maxi;
+    }
+}

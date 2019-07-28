105ad6048f23d35cbbbba5c82943378f570a8625
henryxiao <fortbox1988@outlook.com>
2019-07-28 19:53:14 +0800

    LeetCode 867 Java by Henry Xiao

---
+++ src/No867_转置矩阵/Solution.java
@@ -0,0 +1,14 @@
+package No867_转置矩阵;
+
+class Solution {
+    public int[][] transpose(int[][] A) {
+        int[][] a = new int[A[0].length][A.length];
+        for (int j = 0; j < A[0].length; j++) {
+            for (int i = 0; i < A.length; i++) {
+                a[j][i] = A[i][j];
+            }
+        }
+        return a;
+    }
+
+}

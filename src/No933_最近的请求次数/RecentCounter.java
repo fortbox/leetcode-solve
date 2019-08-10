37d446c2ab5ced7f20e4e64234e72d5cb9de30bc
henryxiao <fortbox1988@outlook.com>
2019-08-10 21:33:15 +0800

    LeetCode 933 Java by Henry Xiao

---
+++ src/No933_最近的请求次数/RecentCounter.java
@@ -0,0 +1,24 @@
+package No933_最近的请求次数;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+class RecentCounter {
+    Queue<Integer> queue;
+
+    public RecentCounter() {
+        queue = new PriorityQueue();
+    }
+
+    public int ping(int t) {
+        while (queue.size() > 0 && queue.peek() < t - 3000) queue.poll();
+        queue.offer(t);
+        return queue.size();
+    }
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.ping(t);
+ */

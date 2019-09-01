e9c9bcb0730082ac15f4a43ba32b0939007239be
henryxiao <fortbox1988@outlook.com>
2019-09-01 17:21:16 +0800

    LeetCode 961 Java by Henry Xiao

---
+++ src/No961_重复N次的元素/Solution.java
@@ -0,0 +1,13 @@
+package No961_重复N次的元素;
+
+import java.util.HashSet;
+
+class Solution {
+    public int repeatedNTimes(int[] A) {
+        HashSet<Integer> hashSet = new HashSet<>();
+        for (int i : A) {
+            if (!hashSet.add(i)) return i;
+        }
+        return 0;
+    }
+}

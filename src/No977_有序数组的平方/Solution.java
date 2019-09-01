b3b01683f3f46676cede004922db211584c9137b
henryxiao <fortbox1988@outlook.com>
2019-09-01 18:10:49 +0800

    LeetCode 977 Java by Henry Xiao

---
+++ src/No977_有序数组的平方/Solution.java
@@ -0,0 +1,14 @@
+package No977_有序数组的平方;
+
+import java.util.Arrays;
+
+class Solution {
+    public int[] sortedSquares(int[] A) {
+        //平方然后再排序
+        for (int i = 0; i < A.length; i++) {
+            A[i] = A[i] * A[i];
+        }
+        Arrays.parallelSort(A);
+        return A;
+    }
+}

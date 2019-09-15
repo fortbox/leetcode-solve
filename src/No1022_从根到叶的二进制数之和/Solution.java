e567cbc907c025b593a4aa6de2a8e55fa233d860
henryxiao <fortbox1988@outlook.com>
2019-09-15 12:45:49 +0800

    项目结构调整

--- src/No1022_从根到叶的二进制数之和/Solution.java
+++ src/No1022_从根到叶的二进制数之和/Solution.java
@@ -3,10 +3,10 @@ package ToDo.No1022_从根到叶的二进制数之和;
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
  * }
  */
 class Solution {

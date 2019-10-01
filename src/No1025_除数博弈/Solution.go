7b61c66af7457d255cfae07581475ee61b181b78
henryxiao <fortbox1988@outlook.com>
2019-10-01 13:55:42 +0800

    LeetCode 1025 Java Python3 Go by Henry Xiao

---
+++ src/No1025_除数博弈/Solution.go
@@ -0,0 +1,5 @@
+package No1025_除数博弈
+
+func divisorGame(N int) bool {
+	return N%2 == 0
+}
--- src/No1025_除数博弈/Solution.java
+++ src/No1025_除数博弈/Solution.java
@@ -1,7 +1,7 @@
-package ToDo.No1025_除数博弈;
+package No1025_除数博弈;
 
 class Solution {
     public boolean divisorGame(int N) {
-
+        return N % 2 == 0;
     }
 }

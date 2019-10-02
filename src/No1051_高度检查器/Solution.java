68665d39288711c04dd79a7b52feb055fce137f3
henryxiao <fortbox1988@outlook.com>
2019-10-02 19:19:37 +0800

    LeetCode 1051 Java by Henry Xiao

--- src/No1051_高度检查器/Solution.java
+++
@@ -1,7 +0,0 @@
-package ToDo.No1051_高度检查器;
-
-class Solution {
-    public int heightChecker(int[] heights) {
-
-    }
-}
---
+++ src/finished/No1051_高度检查器/Solution.java
@@ -0,0 +1,21 @@
+package finished.No1051_高度检查器;
+
+import java.util.Arrays;
+
+class Solution {
+    public int heightChecker(int[] heights) {
+        int count = 0;
+        int[] arr = new int[heights.length];
+        for (int i = 0; i < heights.length; i++) {
+            arr[i] = heights[i];
+        }
+
+        Arrays.sort(heights);
+        for (int i = 0; i < heights.length; i++) {
+            if (arr[i] != heights[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+}

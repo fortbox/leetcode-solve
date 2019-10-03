c4698e6f4218be82955a7d957027b6e3aad257e3
henryxiao <fortbox1988@outlook.com>
2019-10-03 17:36:33 +0800

    LeetCode 1176 Java by Henry Xiao

---
+++ src/finished/No1176_健身计划评估/Solution.java
@@ -0,0 +1,20 @@
+package finished.No1176_健身计划评估;
+
+class Solution {
+    public int dietPlanPerformance(int[] calories, int k, int lower, int upper) {
+        int score = 0;
+        for (int i = 0; i < calories.length - k + 1; i++) {
+            int calo = 0;
+            for (int j = i; j < calories.length && j < i + k; j++) {
+                calo += calories[j];
+            }
+            if (calo < lower) {
+                score--;
+            }
+            if (calo > upper) {
+                score++;
+            }
+        }
+        return score;
+    }
+}

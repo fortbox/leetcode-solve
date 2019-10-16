75cc2cf7297f72658e747d944ac0bfe07d3ef1a8
henryxiao <fortbox1988@outlook.com>
2019-10-17 01:02:02 +0800

    LeetCode greedy 45 Java by Henry Xiao

---
+++ src/greedy/No45_跳跃游戏II/Solution.java
@@ -0,0 +1,21 @@
+package greedy.No45_跳跃游戏II;
+
+class Solution {
+    /**
+     * 往前跳，然后对比获取最远的值。
+     *
+     * @param nums
+     * @return
+     */
+    public int jump(int[] nums) {
+        int len = nums.length, farest = 0, nextStart = 0, steps = 0;
+        for (int i = 0; i < len - 1; i++) {
+            farest = Math.max(farest, i + nums[i]);
+            if (i == nextStart) {
+                nextStart = farest;
+                steps++;
+            }
+        }
+        return steps;
+    }
+}

de5271c000469967eda611f7d0bbd6c832529c73
henryxiao <fortbox1988@outlook.com>
2019-10-18 00:31:08 +0800

    LeetCode greedy 55 Java by Henry Xiao

---
+++ src/greedy/No55_跳跃游戏/Solution.java
@@ -0,0 +1,21 @@
+package greedy.No55_跳跃游戏;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 1, 0, 4};
+        System.out.println("canJump(nums) = " + canJump(nums));
+    }
+
+    public static boolean canJump(int[] nums) {
+        int far = 0, next = 0, n = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            far = Math.max(far, i + nums[i]);
+            if (i == next) {
+                n += far - next;
+                next = far;
+
+            }
+        }
+        return n >= nums.length - 1;
+    }
+}

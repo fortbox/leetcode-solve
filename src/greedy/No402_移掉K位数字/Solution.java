58fce02c5f36b62477b3a487243aca57c65f6142
henryxiao <fortbox1988@outlook.com>
2019-10-27 16:00:25 +0800

    LeetCode greedy 402 Java by Henry Xiao

--- src/greedy/No402_移掉K位数字/Solution.java
+++ src/greedy/No402_移掉K位数字/Solution.java
@@ -1,4 +1,32 @@
 package greedy.No402_移掉K位数字;
 
-public class Solution {
+import java.util.Stack;
+
+class Solution {
+    public String removeKdigits(String num, int k) {
+        if (k >= num.length()) {
+            return "0";
+        }
+        Stack<Integer> stack = new Stack<>();
+        int i = 0;
+        for (; i < num.length(); i++) {
+            int val = num.charAt(i) - '0';
+            while (!stack.isEmpty() && k > 0 && stack.peek() > val) {
+                stack.pop();
+                k--;
+            }
+            if (val != 0 || !stack.isEmpty()) {
+                stack.push(val);
+            }
+        }
+        while (k > 0 && !stack.isEmpty()) {
+            stack.pop();
+            k--;
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop().intValue());
+        }
+        return sb.reverse().toString().isEmpty() ? "0" : sb.toString();
+    }
 }

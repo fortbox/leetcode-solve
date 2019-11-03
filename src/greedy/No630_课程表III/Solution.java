a9407b01617ea3c169ebf1b2f1dc3256c4315f39
henryxiao <fortbox1988@outlook.com>
2019-11-03 16:56:39 +0800

    LeetCode greedy 630 Java by Henry Xiao

--- src/greedy/No630_课程表III/Solution.java
+++ src/greedy/No630_课程表III/Solution.java
@@ -1,4 +1,32 @@
 package greedy.No630_课程表III;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[][] courses = {{1, 2}, {2, 3}};
+        int res = solution.scheduleCourse(courses);
+        System.out.println("res = " + res);
+    }
+
+    public int scheduleCourse(int[][] courses) {
+        Arrays.sort(courses, (Comparator.comparingInt(o -> o[1])));
+        PriorityQueue<Integer> queue = new PriorityQueue<>((o1, o2) -> o2 - o1);
+        int day = 0;
+        for (int[] cour : courses) {
+            int a = cour[0];
+            int b = cour[1];
+            if (day + a <= b) {
+                queue.offer(a);
+                day += a;
+            } else if (!queue.isEmpty() && queue.peek() > a) {
+                day += (a - queue.poll());
+                queue.offer(a);
+            }
+        }
+        return queue.size();
+    }
 }

0d8b6803e9c231f86cce09f2cdf69761b09a7432
henryxiao <fortbox1988@outlook.com>
2019-11-17 16:36:07 +0800

    LeetCode greedy 881 Java by Henry Xiao

--- src/greedy/No881_救生艇/Solution.java
+++ src/greedy/No881_救生艇/Solution.java
@@ -1,4 +1,26 @@
 package greedy.No881_救生艇;
 
+import java.util.Arrays;
+
 public class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int i = 0, j = people.length - 1;
+        int res = 0;
+        while (i < j) {
+            if (people[i] + people[j] <= limit) {
+                i++;
+                j--;
+                res++;
+            } else if (people[i] + people[j] > limit) {
+                j--;
+                res++;
+            }
+            if (i == j) {
+                res++;
+                break;
+            }
+        }
+        return res;
+    }
 }

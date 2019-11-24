ecb165d3700eaed04ea72e2cd424dfb60d606aff
henryxiao <fortbox1988@outlook.com>
2019-11-24 18:53:28 +0800

    LeetCode greedy 995 Java by Henry Xiao

--- src/greedy/No995_k连续位的最小翻转次数/Solution.java
+++ src/greedy/No995_k连续位的最小翻转次数/Solution.java
@@ -1,4 +1,20 @@
 package greedy.No995_k连续位的最小翻转次数;
 
 public class Solution {
+    public int minKBitFlips(int[] A, int K) {
+        int res = 0;
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] == 0) {
+                for (int j = i; j < i + K; j++) {
+                    if (i + K <= A.length) {
+                        A[j] ^= 1;
+                    } else {
+                        return -1;
+                    }
+                }
+                res++;
+            }
+        }
+        return res;
+    }
 }

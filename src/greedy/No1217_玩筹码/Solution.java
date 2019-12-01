bba140932d1b6c3a1c255c9c5a3267cbd4f8aa7e
henryxiao <fortbox1988@outlook.com>
2019-12-01 19:29:12 +0800

    LeetCode greedy 1217 Java by Henry Xiao

--- src/greedy/No1217_玩筹码/Solution.java
+++ src/greedy/No1217_玩筹码/Solution.java
@@ -1,4 +1,16 @@
 package greedy.No1217_玩筹码;
 
 public class Solution {
+    public int minCostToMoveChips(int[] chips) {
+        int a = 0;
+        int b = 0;
+        for (int chip : chips) {
+            if (chip % 2 == 0) {
+                a++;
+            } else {
+                b++;
+            }
+        }
+        return Math.min(a, b);
+    }
 }

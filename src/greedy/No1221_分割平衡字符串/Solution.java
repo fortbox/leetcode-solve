769ac36984460a828033bd121daa9d2e32b2c42b
henryxiao <fortbox1988@outlook.com>
2019-12-01 19:37:59 +0800

    LeetCode greedy 1221Java by Henry Xiao

--- src/greedy/No1221_分割平衡字符串/Solution.java
+++ src/greedy/No1221_分割平衡字符串/Solution.java
@@ -1,4 +1,35 @@
 package greedy.No1221_分割平衡字符串;
 
 public class Solution {
+    public int balancedStringSplit(String s) {
+        int res = 0;
+        char[] array = s.toCharArray();
+        int index_R = 0;
+        int index_L = 0;
+        for (char c : array) {
+            if (c == 'R') {
+                if (index_L > 1) {
+                    index_L--;
+                } else if (index_L == 1) {
+                    index_L = 0;
+                    index_R = 0;
+                    res++;
+                } else if (index_L == 0) {
+                    index_R++;
+                }
+            } else {
+                if (index_R > 1) {
+                    index_R--;
+                } else if (index_R == 1) {
+                    index_R = 0;
+                    index_L = 0;
+                    res++;
+                } else if (index_R == 0) {
+                    index_L++;
+                }
+
+            }
+        }
+        return res;
+    }
 }

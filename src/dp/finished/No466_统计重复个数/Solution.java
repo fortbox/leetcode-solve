9f2955bcf0296ec9d506368ec9fb5ea2b13825a7
henryxiao <fortbox1988@outlook.com>
2020-01-31 20:53:07 +0800

    项目结构调整

--- src/dp/finished/No466_统计重复个数/Solution.java
+++ src/dp/finished/No466_统计重复个数/Solution.java
@@ -3,7 +3,7 @@
  * @Author:xiaoweixiang
  */
 
-package dp.No466_统计重复个数;
+package dp.finished.No466_统计重复个数;
 
 public class Solution {
     public int getMaxRepetitions(String s1, int n1, String s2, int n2) {

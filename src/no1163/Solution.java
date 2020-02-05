b9521c9737949ea2772b0c2edc69cf31e255650b
henryxiao <fortbox1988@outlook.com>
2020-02-05 14:52:55 +0800

    LeetCode dp 1163 Java by Henry Xiao

---
+++ src/no1163/Solution.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2020
+ * @Author:xiaoweixiang
+ */
+
+
+public class Solution {
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        String s = "cacac";
+        String result = lastSubstring(s);
+        long end = System.currentTimeMillis();
+        System.out.println("(end-start) = " + (end - start));
+        System.out.println("result = " + result);
+    }
+
+    /**
+     * 减少substring就行
+     *
+     * @param s
+     * @return
+     */
+    public static String lastSubstring(String s) {
+        int st = 0;
+        int len = s.length();
+        for (int i = 1; i < len; i++) {
+            int sz = 0;
+            while (i + sz < len && s.charAt(st + sz) == s.charAt(i + sz)) {
+                sz++;
+            }
+            System.out.println("i = " + i);
+            System.out.println("sz = " + sz);
+            if (i + sz == len) {
+                break;
+            }
+            if (s.charAt(i + sz) > s.charAt(st + sz)) {
+                st = i;
+            }
+
+        }
+        return s.substring(st);
+    }
+}

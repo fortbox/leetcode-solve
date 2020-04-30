931e1de18181280006ae3b53e497e6ff065db770
henryxiao <fortbox1988@hotmail.com>
2020-05-01 00:20:56 +0800

    LeetCode 663 Java solve by Henry

---
+++ src/main/java/no1245/Solution.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2020
+ * Author: xiaoweixiang
+ */
+
+package no1245;
+
+import java.util.*;
+
+class Solution {
+    private TreeVertex root = new TreeVertex(0);
+
+    public int treeDiameter(int[][] edges) {
+        if (edges.length == 0) return 0;
+        Map<Integer, TreeVertex> treeVertexMap = initVertexCache();
+        for (int i = 0; i < edges.length; i++) {
+            TreeVertex vertex = getTreeVertex(treeVertexMap, edges[i][0]);
+            TreeVertex childVertex = getTreeVertex(treeVertexMap, edges[i][1]);
+            vertex.child.add(childVertex);
+        }
+        int[] maxDiameter = new int[]{0};
+        getMaxDiameter(maxDiameter, root);
+        return maxDiameter[0];
+    }
+
+    private Map<Integer, TreeVertex> initVertexCache() {
+        HashMap<Integer, TreeVertex> treeVertexMap = new HashMap<>();
+        treeVertexMap.put(0, root);
+        return treeVertexMap;
+    }
+
+    private TreeVertex getTreeVertex(Map<Integer, TreeVertex> treeVertexMap, int index) {
+        TreeVertex vertex = treeVertexMap.get(index);
+        if (vertex == null) {
+            vertex = new TreeVertex(index);
+            treeVertexMap.put(index, vertex);
+        }
+        return vertex;
+    }
+
+    private int getMaxDiameter(int[] maxDiameter, TreeVertex vertex) {
+        if (vertex == null) return 0;
+        int curVertexDiameter = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(2);
+        for (TreeVertex temp : vertex.child) {
+            int tempDiameter = getMaxDiameter(maxDiameter, temp);
+            curVertexDiameter = Math.max(curVertexDiameter, tempDiameter);
+            if (pq.size() == 2) {
+                if (tempDiameter > pq.peek()) {
+                    pq.poll();
+                    pq.offer(tempDiameter);
+                }
+            } else {
+                pq.offer(tempDiameter);
+            }
+        }
+        int sumDiameter = 0;
+        while (!pq.isEmpty()) {
+            sumDiameter += pq.poll();
+        }
+        maxDiameter[0] = Math.max(maxDiameter[0], sumDiameter);
+        return 1 + curVertexDiameter;
+    }
+
+    private static class TreeVertex {
+        int index;
+        List<TreeVertex> child;
+
+        TreeVertex(int index) {
+            this.index = index;
+            this.child = new ArrayList<>();
+        }
+    }
+}
---
+++ src/main/java/no663/Solution.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2020
+ * Author: xiaoweixiang
+ */
+
+package no663;
+
+
+import java.util.ArrayList;
+
+class Solution {
+    public boolean checkEqualTree(TreeNode root) {
+        /**
+         * 1，求树的所有节点的和
+         * 2，遍历每个节点，找出他的左节点子树的和，右节点子树的和。
+         * 3，判断。
+         */
+        int total = sumAllNode(root);
+        ArrayList<Integer> list = new ArrayList<>();
+        preOrder(root, list);
+        for (Integer integer : list) {
+            if (integer * 2 == total) return true;
+        }
+        return false;
+    }
+
+    private void preOrder(TreeNode root, ArrayList<Integer> list) {
+        if (root == null) return;
+        if (root.left != null) {
+            list.add(sumAllNode(root.left));
+        }
+        if (root.right != null) {
+            list.add(sumAllNode(root.right));
+        }
+        preOrder(root.left, list);
+        preOrder(root.right, list);
+    }
+
+    private int sumAllNode(TreeNode root) {
+        if (root == null) return 0;
+        return root.val + sumAllNode(root.left) + sumAllNode(root.right);
+    }
+}

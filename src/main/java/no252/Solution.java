14074bc62b32cc9ef7bc3645b76215a2ca36fcc7
henryxiao <fortbox1988@hotmail.com>
2020-05-02 00:20:25 +0800

    LeetCode 252 Java solve by Henry

---
+++ src/main/java/no252/Solution.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2020
+ * Author: xiaoweixiang
+ */
+
+package no252;
+
+import java.util.Arrays;
+
+class Solution {
+    public boolean canAttendMeetings(int[][] intervals) {
+        Arrays.sort(intervals, (o1, o2) -> o1[0] - o2[0]);
+        for (int i = 1; i < intervals.length; i++) {
+            int[] a = intervals[i - 1];
+            int[] b = intervals[i];
+            if (a[1] > b[0]) return false;
+        }
+        return true;
+    }
+}

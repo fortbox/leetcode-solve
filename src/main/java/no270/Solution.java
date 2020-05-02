890c3522ec6e816e3a73e31a35208d8e71131e0f
henryxiao <fortbox1988@hotmail.com>
2020-05-02 15:15:26 +0800

    LeetCode 270 Java solve by Henry

---
+++ src/main/java/no270/Solution.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2020
+ * Author: xiaoweixiang
+ */
+
+package no270;
+
+class Solution {
+    public int res = Integer.MAX_VALUE;
+    public double init = Integer.MAX_VALUE;
+
+    public int closestValue(TreeNode root, double target) {
+        dfs(root, target);
+        return res;
+    }
+
+    private void dfs(TreeNode root, double target) {
+        if (root == null) return;
+        int val = root.val;
+        double dif = Math.abs(target - val);
+        if (init > dif) {
+            init = dif;
+            res = val;
+        }
+        dfs(root.left, target);
+        dfs(root.right, target);
+    }
+}

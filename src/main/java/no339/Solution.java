c0c91b251ab95ddb7e7224a7f5985b13314a28e6
henryxiao <fortbox1988@hotmail.com>
2020-05-02 17:38:22 +0800

    LeetCode 339 Java solve by Henry

--- src/main/java/no339/Solution.java
+++ src/main/java/no339/Solution.java
@@ -5,5 +5,25 @@
 
 package no339;
 
-public class Solution {
+import java.util.List;
+
+class Solution {
+    public int depthSum(List<NestedInteger> nestedList) {
+        /**
+         * 看评论才理解这道题的题目
+         */
+        return depthSum(nestedList, 1);
+    }
+
+    private int depthSum(List<NestedInteger> nestedList, int deep) {
+        int sum = 0;
+        for (NestedInteger i : nestedList) {
+            if (i.isInteger()) {
+                sum += i.getInteger() * deep;
+                continue;
+            }
+            sum += depthSum(i.getList(), deep + 1);
+        }
+        return sum;
+    }
 }

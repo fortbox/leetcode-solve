68ba9c9b40b5edb6bee719291f8db9a400b624ec
henryxiao <fortbox1988@hotmail.com>
2020-05-04 10:58:57 +0800

    LeetCode 760 Java solve by Henry

--- src/main/java/no760/Solution.java
+++ src/main/java/no760/Solution.java
@@ -5,5 +5,27 @@
 
 package no760;
 
-public class Solution {
+import java.util.ArrayList;
+
+class Solution {
+    public int[] anagramMappings(int[] A, int[] B) {
+        ArrayList<Integer> listA = new ArrayList<>();
+        ArrayList<Integer> listB = new ArrayList<>();
+        for (int i : A) {
+            listA.add(i);
+        }
+        for (int i : B) {
+            listB.add(i);
+        }
+        ArrayList<Integer> list = new ArrayList<>();
+        for (Integer n : listA) {
+            list.add(listB.indexOf(n));
+        }
+        int[] res = new int[A.length];
+
+        for (int i = 0; i < res.length; i++) {
+            res[i] = list.get(i);
+        }
+        return res;
+    }
 }

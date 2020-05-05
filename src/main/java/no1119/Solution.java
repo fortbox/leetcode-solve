6c9adc85037b290c621af92d27cd01fc8bb22838
henryxiao <fortbox1988@hotmail.com>
2020-05-05 21:00:02 +0800

    LeetCode 1119 Java solve by Henry

--- src/main/java/no1119/Solution.java
+++ src/main/java/no1119/Solution.java
@@ -5,5 +5,16 @@
 
 package no1119;
 
-public class Solution {
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public String removeVowels(String S) {
+        StringBuilder builder = new StringBuilder();
+        List<Character> list = Arrays.asList('a', 'e', 'i', 'o', 'u');
+        for (int i = 0; i < S.length(); i++) {
+            if (!list.contains(S.charAt(i))) builder.append(S.charAt(i));
+        }
+        return builder.toString();
+    }
 }

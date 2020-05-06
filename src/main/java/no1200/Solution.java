56c760b7cad824751380556ba14461c3248c2240
henryxiao <fortbox1988@hotmail.com>
2020-05-07 00:25:14 +0800

    LeetCode 1200 Java solve by Henry

--- src/main/java/no1200/Solution.java
+++ src/main/java/no1200/Solution.java
@@ -5,5 +5,25 @@
 
 package no1200;
 
-public class Solution {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> minimumAbsDifference(int[] arr) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(arr);
+        int dif = Integer.MAX_VALUE;
+        for (int i = 1; i < arr.length; i++) {
+            int tmp = arr[i] - arr[i - 1];
+            if (tmp < dif) dif = tmp;
+        }
+        for (int i = 1; i < arr.length; i++) {
+            int tmp = arr[i] - arr[i - 1];
+            if (tmp == dif) {
+                list.add(Arrays.asList(arr[i - 1], arr[i]));
+            }
+        }
+        return list;
+    }
 }

68fb323134f383caf5712ea75563f3c454a12452
henryxiao <fortbox1988@hotmail.com>
2020-05-07 00:03:15 +0800

    LeetCode 1180 Java solve by Henry

--- src/main/java/no1180/Solution.java
+++ src/main/java/no1180/Solution.java
@@ -5,5 +5,16 @@
 
 package no1180;
 
-public class Solution {
+class Solution {
+    public static int countLetters(String S) {
+        int ans = 0, count = 1;
+        for (int i = 1; i <= S.length(); i++) {
+            if (i < S.length() && S.charAt(i - 1) == S.charAt(i)) count++;
+            else {
+                ans += (1 + count) * count / 2;
+                count = 1;
+            }
+        }
+        return ans;
+    }
 }

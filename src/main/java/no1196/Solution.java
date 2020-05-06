3bf4cdd58d420cf4a2b90ee1932b2c8dfe3a1fb1
henryxiao <fortbox1988@hotmail.com>
2020-05-07 00:16:31 +0800

    LeetCode 1196 Java solve by Henry

--- src/main/java/no1196/Solution.java
+++ src/main/java/no1196/Solution.java
@@ -5,5 +5,20 @@
 
 package no1196;
 
-public class Solution {
+import java.util.Arrays;
+
+class Solution {
+    public int maxNumberOfApples(int[] arr) {
+        Arrays.sort(arr);
+        int ans = 0;
+        int sum = 0;
+        for (int i : arr) {
+            sum += i;
+            if (sum > 5000) {
+                return ans;
+            }
+            ans++;
+        }
+        return ans;
+    }
 }

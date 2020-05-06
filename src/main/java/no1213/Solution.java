ae15b1c33f90a47ae4738ae8fe14f164b80a90a7
henryxiao <fortbox1988@hotmail.com>
2020-05-07 00:31:52 +0800

    LeetCode 1213 Java solve by Henry

--- src/main/java/no1213/Solution.java
+++ src/main/java/no1213/Solution.java
@@ -5,5 +5,18 @@
 
 package no1213;
 
-public class Solution {
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+class Solution {
+    public List<Integer> arraysIntersection(int[] arr1, int[] arr2, int[] arr3) {
+        ArrayList<Integer> list = new ArrayList<>();
+        HashSet<Integer> set1 = new HashSet<>();
+        HashSet<Integer> set2 = new HashSet<>();
+        for (int i : arr1) set1.add(i);
+        for (int i : arr2) set2.add(i);
+        for (int i : arr3) if (set1.contains(i) && set2.contains(i)) list.add(i);
+        return list;
+    }
 }

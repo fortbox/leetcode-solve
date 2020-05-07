cec2c21706214185baa0cccb21d0c0f87f6780cc
henryxiao <fortbox1988@hotmail.com>
2020-05-08 00:07:46 +0800

    LeetCode 1228 Java solve by Henry

--- src/main/java/no1228/Solution.java
+++ src/main/java/no1228/Solution.java
@@ -5,5 +5,15 @@
 
 package no1228;
 
-public class Solution {
+class Solution {
+    public int missingNumber(int[] arr) {
+        int pre = 0;
+        for (int i = 1; i < arr.length; i++) {
+            int gap = arr[i] - arr[i - 1];
+            if (gap == 2 * pre) return arr[i] - gap / 2;
+            if (pre == 2 * gap) return arr[i - 1] - gap / 2;
+            pre = gap;
+        }
+        return -1;
+    }
 }

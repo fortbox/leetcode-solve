4ec5c16ad123c7ce37faa963b0fda66f116f1d8a
henryxiao <fortbox1988@hotmail.com>
2020-05-09 01:15:47 +0800

    LeetCode 1260 Java solve by Henry

--- src/main/java/no1260/Solution.java
+++ src/main/java/no1260/Solution.java
@@ -5,5 +5,34 @@
 
 package no1260;
 
-public class Solution {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class Solution {
+    public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+        // 每次记住最后一列
+        int row = grid.length;
+        int col = grid[0].length;
+        while (k > 0) {
+            int[] lastCol = new int[row];
+            for (int i = 0; i < row; i++) {
+                lastCol[i] = grid[i][col - 1];
+            }
+            for (int i = 0; i < row; i++) {
+                for (int j = col - 1; j > 0; j--) {
+                    grid[i][j] = grid[i][j - 1];
+                }
+            }
+            for (int i = 1; i < row; i++) {
+                grid[i][0] = lastCol[i - 1];
+            }
+            grid[0][0] = lastCol[row - 1];
+            k--;
+        }
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.stream(grid).forEach(ints -> list.add(Arrays.stream(ints).boxed().collect(Collectors.toList())));
+        return list;
+    }
 }

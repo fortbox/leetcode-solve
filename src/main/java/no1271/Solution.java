a6769845e61efe3ba917fbb213e5a54a922b1659
henryxiao <fortbox1988@hotmail.com>
2020-05-09 01:52:33 +0800

    LeetCode 1271 Java solve by Henry

--- src/main/java/no1271/Solution.java
+++ src/main/java/no1271/Solution.java
@@ -5,5 +5,18 @@
 
 package no1271;
 
-public class Solution {
+import java.util.Arrays;
+import java.util.HashSet;
+
+class Solution {
+    public String toHexspeak(String num) {
+        String ss = Long.toHexString(Long.parseLong(num)).replace('1', 'I').replace('0', 'O').toUpperCase();
+        HashSet<Character> set = new HashSet<>(Arrays.asList('A', 'B', 'C', 'D', 'E', 'F', 'I', 'O'));
+        System.out.println("ss = " + ss);
+        for (int i = 0; i < ss.length(); i++) {
+            Character c = ss.charAt(i);
+            if (!set.contains(c)) return "ERROR";
+        }
+        return ss;
+    }
 }

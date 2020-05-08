dd2fb2c8cb5b2f1ff8887eb8888e4b5289c29ea3
henryxiao <fortbox1988@hotmail.com>
2020-05-09 00:59:19 +0800

    LeetCode 1252 Java solve by Henry

--- src/main/java/no1252/Solution.java
+++ src/main/java/no1252/Solution.java
@@ -5,5 +5,25 @@
 
 package no1252;
 
-public class Solution {
+class Solution {
+    public int oddCells(int n, int m, int[][] indices) {
+        int[][] array = new int[n][m];
+        for (int[] arr : indices) {
+            int row = arr[0];
+            int col = arr[1];
+            for (int i = 0; i < m; i++) {
+                array[row][i]++;
+            }
+            for (int i = 0; i < n; i++) {
+                array[i][col]++;
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[0].length; j++) {
+                if (array[i][j] % 2 == 1) ans++;
+            }
+        }
+        return ans;
+    }
 }

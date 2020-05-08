045d2dd5e1c4fef276f2b5fe822682baec6effab
henryxiao <fortbox1988@hotmail.com>
2020-05-09 01:38:23 +0800

    LeetCode 1266 Java solve by Henry

--- src/main/java/no1266/Solution.java
+++ src/main/java/no1266/Solution.java
@@ -5,5 +5,15 @@
 
 package no1266;
 
-public class Solution {
+class Solution {
+    public int minTimeToVisitAllPoints(int[][] points) {
+        int step = 0;
+        for (int i = 1; i < points.length; i++) {
+            int x = points[i][0] - points[i - 1][0];
+            int y = points[i][1] - points[i - 1][1];
+            System.out.println("x = " + x + ", y = " + y);
+            step += Math.max(Math.abs(x), Math.abs(y));
+        }
+        return step;
+    }
 }

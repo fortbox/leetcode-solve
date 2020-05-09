3adde4a948633b5d55285942850a818e648361dc
henryxiao <fortbox1988@hotmail.com>
2020-05-10 00:34:59 +0800

    LeetCode 1287 Java solve by Henry

--- src/main/java/no1287/Solution.java
+++ src/main/java/no1287/Solution.java
@@ -5,5 +5,17 @@
 
 package no1287;
 
-public class Solution {
+import java.util.HashMap;
+
+class Solution {
+    public int findSpecialInteger(int[] arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int n = arr.length / 4;
+        for (int i : arr) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+            Integer num = map.getOrDefault(i, 0);
+            if (num > n) return i;
+        }
+        return -1;
+    }
 }

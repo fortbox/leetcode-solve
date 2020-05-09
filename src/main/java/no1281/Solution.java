ec83f5813728c9786581c14bc6e351fbb5e1e58a
henryxiao <fortbox1988@hotmail.com>
2020-05-10 00:24:23 +0800

    LeetCode 1281 Java solve by Henry

--- src/main/java/no1281/Solution.java
+++ src/main/java/no1281/Solution.java
@@ -5,5 +5,14 @@
 
 package no1281;
 
-public class Solution {
+class Solution {
+    public int subtractProductAndSum(int n) {
+        int p = 1, a = 0;
+        for (char c : String.valueOf(n).toCharArray()) {
+            int t = c - '0';
+            p *= t;
+            a += t;
+        }
+        return p - a;
+    }
 }

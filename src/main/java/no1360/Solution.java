be0d771560c281e9c1d01bbc13433ead52bf82e9
henryxiao <fortbox1988@hotmail.com>
2020-05-11 00:24:11 +0800

    LeetCode 1360 Java solve by Henry

--- src/main/java/no1360/Solution.java
+++ src/main/java/no1360/Solution.java
@@ -5,5 +5,20 @@
 
 package no1360;
 
-public class Solution {
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+class Solution {
+    public static void main(String[] args) {
+        String s1 = "2019-06-29";
+        String s2 = "2019-06-30";
+        int dif = daysBetweenDates(s1, s2);
+        System.out.println("dif = " + dif);
+
+    }
+
+    public static int daysBetweenDates(String date1, String date2) {
+        return (int) ChronoUnit.DAYS.between(LocalDate.parse(date1, DateTimeFormatter.ISO_LOCAL_DATE), LocalDate.parse(date2, DateTimeFormatter.ISO_LOCAL_DATE));
+    }
 }

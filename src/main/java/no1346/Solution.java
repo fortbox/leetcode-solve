7dd5a33b87fa18df5f2d9486227e755674344352
henryxiao <fortbox1988@hotmail.com>
2020-05-10 23:35:30 +0800

    LeetCode 1346 Java solve by Henry

--- src/main/java/no1346/Solution.java
+++ src/main/java/no1346/Solution.java
@@ -5,5 +5,21 @@
 
 package no1346;
 
-public class Solution {
+import java.util.Arrays;
+import java.util.HashSet;
+
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i : arr) set.add(i);
+        for (int i : arr) {
+            if (i != 0 && set.contains(2 * i)) {
+                return true;
+            }
+            if (i == 0 && Arrays.stream(arr).filter(n -> n == 0).count() > 1) {
+                return true;
+            }
+        }
+        return false;
+    }
 }

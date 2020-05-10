38eab941b9e4437d6477d8a253c554dc89d41b7e
henryxiao <fortbox1988@hotmail.com>
2020-05-10 15:04:30 +0800

    LeetCode 1304 Java solve by Henry

--- src/main/java/no1304/Solution.java
+++ src/main/java/no1304/Solution.java
@@ -5,5 +5,15 @@
 
 package no1304;
 
-public class Solution {
+class Solution {
+    public int[] sumZero(int n) {
+        int[] ans = new int[n];
+        int index = 0;
+        for (int i = 1; i <= n / 2; i++) {
+            ans[index++] = i;
+            ans[index++] = -1;
+        }
+        if (n % 2 == 1) ans[index] = 0;
+        return ans;
+    }
 }

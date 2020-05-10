02e014ebee09aecae88f25c612ab8a971fb17139
henryxiao <fortbox1988@hotmail.com>
2020-05-10 15:25:03 +0800

    LeetCode 1309 Java solve by Henry

--- src/main/java/no1309/Solution.java
+++ src/main/java/no1309/Solution.java
@@ -5,5 +5,37 @@
 
 package no1309;
 
-public class Solution {
+class Solution {
+    public String freqAlphabets(String s) {
+        if (s == null || "".equals(s)) return "";
+        int index = s.indexOf('#');
+        String s1 = null;
+        if (index != -1) {
+            s1 = buildStr(s.substring(0, index));
+            return s1 + freqAlphabets(s.substring(index + 1));
+        } else {
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+                int ci = c - '1' + 'a';
+                builder.append((char) ci);
+            }
+            s1 = builder.toString();
+            return s1;
+        }
+    }
+
+    private String buildStr(String s) {
+        String s1 = s.substring(s.length() - 2);
+        int n = Integer.parseInt(s1);
+        int nn = 'a' + n - 1;
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < s.length() - 2; i++) {
+            char c = s.charAt(i);
+            int ci = c - '1' + 'a';
+            builder.append((char) ci);
+        }
+        builder.append((char) nn);
+        return builder.toString();
+    }
 }

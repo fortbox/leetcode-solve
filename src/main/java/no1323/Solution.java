f157b4428b39899f19df6fc8292e0efd6d17ac63
henryxiao <fortbox1988@hotmail.com>
2020-05-10 15:50:14 +0800

    LeetCode 1323 Java solve by Henry

--- src/main/java/no1323/Solution.java
+++ src/main/java/no1323/Solution.java
@@ -5,5 +5,8 @@
 
 package no1323;
 
-public class Solution {
+class Solution {
+    public int maximum69Number(int num) {
+        return Integer.parseInt(String.valueOf(num).replaceFirst("6", "9"));
+    }
 }

0fb916deeaee47d5c63d712d01647e5f69ae1c07
henryxiao <fortbox1988@hotmail.com>
2020-05-10 14:15:45 +0800

    LeetCode 1295 Java solve by Henry

--- src/main/java/no1295/Solution.java
+++ src/main/java/no1295/Solution.java
@@ -5,5 +5,10 @@
 
 package no1295;
 
-public class Solution {
+import java.util.Arrays;
+
+class Solution {
+    public int findNumbers(int[] nums) {
+        return (int) Arrays.stream(nums).mapToObj(String::valueOf).filter(s -> s.length() % 2 == 1).count();
+    }
 }

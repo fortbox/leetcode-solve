e62dc3c2649ae7130e0ae3b0a1f122faea443165
henryxiao <fortbox1988@hotmail.com>
2020-05-10 23:15:48 +0800

    LeetCode 1337 Java solve by Henry

--- src/main/java/no1337/Solution.java
+++ src/main/java/no1337/Solution.java
@@ -5,5 +5,19 @@
 
 package no1337;
 
-public class Solution {
+import java.util.Arrays;
+
+class Solution {
+    public int[] kWeakestRows(int[][] mat, int k) {
+        int[][] res = new int[mat.length][2];
+        for (int i = 0; i < mat.length; i++) {
+            int count = (int) Arrays.stream(mat[i]).filter(j -> j == 1).count();
+            res[i][0] = count;
+            res[i][1] = i;
+        }
+        Arrays.sort(res, (o1, o2) -> o1[0] == o2[0] ? o1[1] - o2[1] : o1[0] - o2[0]);
+        int[] answer = new int[k];
+        for (int i = 0; i < k; i++) answer[i] = res[i][1];
+        return answer;
+    }
 }

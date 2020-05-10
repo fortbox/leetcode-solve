2c8a6f7c94aa1897a9d8f09bc8f18a944ffbae96
henryxiao <fortbox1988@hotmail.com>
2020-05-10 17:35:17 +0800

    LeetCode 1332 Java solve by Henry

--- src/main/java/no1332/Solution.java
+++ src/main/java/no1332/Solution.java
@@ -5,5 +5,17 @@
 
 package no1332;
 
-public class Solution {
+class Solution {
+    public static void main(String[] args) {
+        String s = "abb";
+        int res = removePalindromeSub(s);
+        System.out.println("res = " + res);
+    }
+
+    public static int removePalindromeSub(String s) {
+        if (s.length() == 0) return 0;
+        StringBuilder builder = new StringBuilder(s);
+        if (s.equals(builder.reverse().toString())) return 1;
+        return 2;
+    }
 }

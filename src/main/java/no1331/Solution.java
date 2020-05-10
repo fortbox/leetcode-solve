6efd368575f5fa601656a1d22131549a05ce7065
henryxiao <fortbox1988@hotmail.com>
2020-05-10 17:01:32 +0800

    LeetCode 1331 Java solve by Henry

--- src/main/java/no1331/Solution.java
+++ src/main/java/no1331/Solution.java
@@ -5,5 +5,23 @@
 
 package no1331;
 
-public class Solution {
+import java.util.Arrays;
+import java.util.Map;
+import java.util.TreeMap;
+
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        Map<Integer, Integer> map = new TreeMap<>();
+        int index = 1;
+        int[] ints = Arrays.copyOfRange(arr, 0, arr.length);
+        Arrays.sort(ints);
+        for (int num : ints) {
+            if (map.containsKey(num)) continue;
+            map.put(num, index++);
+        }
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = map.get(arr[i]);
+        }
+        return arr;
+    }
 }

a106bfd96af3959ef24fa4584a5fc8504993ebba
henryxiao <fortbox1988@hotmail.com>
2020-05-10 15:45:03 +0800

    LeetCode 1317 Java solve by Henry

--- src/main/java/no1317/Solution.java
+++ src/main/java/no1317/Solution.java
@@ -5,5 +5,15 @@
 
 package no1317;
 
-public class Solution {
+class Solution {
+    public int[] getNoZeroIntegers(int n) {
+        int[] res = new int[2];
+        int begin = 1;
+        while (true) {
+            res[0] = begin;
+            res[1] = n - begin;
+            if (!(String.valueOf(res[0]).contains("0") || String.valueOf(res[1]).contains("0"))) return res;
+            begin++;
+        }
+    }
 }

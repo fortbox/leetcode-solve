0628bf58d48dd06d428928847bb55ca77ef94bd4
henryxiao <fortbox1988@hotmail.com>
2020-05-10 23:59:22 +0800

    LeetCode 1356 Java solve by Henry

--- src/main/java/no1356/Solution.java
+++ src/main/java/no1356/Solution.java
@@ -5,5 +5,33 @@
 
 package no1356;
 
-public class Solution {
+import java.util.Arrays;
+
+class Solution {
+    public int[] sortByBits(int[] arr) {
+        Integer[] array = Arrays.stream(arr).boxed().toArray(Integer[]::new);
+        Arrays.sort(array, (o1, o2) -> {
+            String s1 = Integer.toBinaryString(o1);
+            String s2 = Integer.toBinaryString(o2);
+            int n1 = 0, n2 = 0;
+            for (int i = 0; i < s1.length(); i++) {
+                if (s1.charAt(i) == '1') n1++;
+            }
+            for (int i = 0; i < s2.length(); i++) {
+                if (s2.charAt(i) == '1') n2++;
+            }
+            if (n1 < n2) {
+                return -1;
+            } else if (n1 > n2) {
+                return 1;
+            } else {
+                return o1 - o2;
+            }
+        });
+        for (int i = 0; i < array.length; i++) {
+            arr[i] = array[i];
+        }
+        return arr;
+
+    }
 }

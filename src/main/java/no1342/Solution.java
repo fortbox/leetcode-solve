779ce3da610b352b8aff807f883a9aa2b296cde2
henryxiao <fortbox1988@hotmail.com>
2020-05-10 23:28:55 +0800

    LeetCode 1342 Java solve by Henry

--- src/main/java/no1342/Solution.java
+++ src/main/java/no1342/Solution.java
@@ -5,5 +5,16 @@
 
 package no1342;
 
-public class Solution {
+class Solution {
+    public static int numberOfSteps(int num) {
+        String s = Integer.toBinaryString(num);
+        char[] chars = s.toCharArray();
+        int n = 0;
+        for (char c : chars) {
+            if (c == '1') {
+                n++;
+            }
+        }
+        return s.length() - 1 + n;
+    }
 }

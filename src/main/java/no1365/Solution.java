ec22ace0dac89870baf7a7d5ad2d3b5281f4e9b8
henryxiao <fortbox1988@hotmail.com>
2020-05-12 00:26:37 +0800

    LeetCode 1365 Java solve by Henry

--- src/main/java/no1365/Solution.java
+++ src/main/java/no1365/Solution.java
@@ -5,5 +5,15 @@
 
 package no1365;
 
-public class Solution {
+import java.util.Arrays;
+
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] res = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            res[i] = (int) Arrays.stream(nums).filter(j -> j < num).count();
+        }
+        return res;
+    }
 }

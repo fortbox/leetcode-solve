39b37e31a9671bff7448503cbf1e122afa8c78f3
henryxiao <fortbox1988@hotmail.com>
2020-05-12 01:32:47 +0800

    LeetCode 1374 Java solve by Henry

--- src/main/java/no1374/Solution.java
+++ src/main/java/no1374/Solution.java
@@ -5,5 +5,22 @@
 
 package no1374;
 
-public class Solution {
+class Solution {
+    public String generateTheString(int n) {
+        StringBuilder builder = new StringBuilder();
+        if (n % 2 == 0) {
+            for (int i = 0; i < n - 1; i++) {
+                builder.append('a');
+            }
+            builder.append('b');
+        } else {
+            int k = n - 1;
+            for (int i = 0; i < n - 1; i++) {
+                builder.append('a');
+            }
+            builder.append('b');
+            builder.append('c');
+        }
+        return builder.toString();
+    }
 }

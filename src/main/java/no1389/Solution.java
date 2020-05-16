03c68e66ed232c9510907201d953c4f7b9d47b18
henryxiao <fortbox1988@hotmail.com>
2020-05-16 20:32:56 +0800

    LeetCode 1389 Java solve by Henry

--- src/main/java/no1389/Solution.java
+++ src/main/java/no1389/Solution.java
@@ -5,5 +5,30 @@
 
 package no1389;
 
-public class Solution {
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int[] createTargetArray(int[] nums, int[] index) {
+        List<Integer> list = new ArrayList<>();
+        int m = nums.length;
+        int n = index.length;
+        int i = 0;
+        int j = 0;
+        while (true) {
+            if (i >= m && j >= n) {
+                break;
+            } else if (i < m && j < n) {
+                list.add(index[j], nums[i]);
+                i++;
+                j++;
+            }
+        }
+        Integer[] array = list.toArray(new Integer[0]);
+        int[] res = new int[array.length];
+        for (int k = 0; k < res.length; k++) {
+            res[k] = array[k];
+        }
+        return res;
+    }
 }

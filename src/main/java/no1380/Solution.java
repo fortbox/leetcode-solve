a7537b179a913cdfdc2ade32ec08d4de6a9127b4
henryxiao <fortbox1988@hotmail.com>
2020-05-16 20:11:16 +0800

    LeetCode 1380 Java solve by Henry

--- src/main/java/no1380/Solution.java
+++ src/main/java/no1380/Solution.java
@@ -5,5 +5,31 @@
 
 package no1380;
 
-public class Solution {
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Integer> luckyNumbers(int[][] matrix) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                int c = matrix[i][j];
+                if (check(matrix, i, j)) {
+                    list.add(c);
+                }
+            }
+        }
+        return list;
+    }
+
+    private boolean check(int[][] matrix, int row, int column) {
+        int n = matrix[row][column];
+        for (int i = 0; i < matrix[0].length; i++) {
+            if (matrix[row][i] < n) return false;
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            if (matrix[i][column] > n) return false;
+        }
+        return true;
+    }
 }

b34901f003797fd638e163168308ca00f9c8dfab
henryxiao <fortbox1988@hotmail.com>
2020-05-16 20:54:52 +0800

    LeetCode 1394 Java solve by Henry

--- src/main/java/no1394/Solution.java
+++ src/main/java/no1394/Solution.java
@@ -5,5 +5,24 @@
 
 package no1394;
 
-public class Solution {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+    public int findLucky(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : arr) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+        List<Integer> list = new ArrayList<>();
+        map.forEach((k, v) -> {
+            if (k.intValue() == v.intValue()) {
+                list.add(k);
+            }
+        });
+        list.sort(Integer::compareTo);
+        return list.size() > 0 ? list.get(list.size() - 1) : -1;
+    }
 }

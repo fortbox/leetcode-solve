f8dbc0aaaa3b125adbddcce24a6f503d7b9a87c9
henryxiao <fortbox1988@hotmail.com>
2020-05-17 16:38:04 +0800

    LeetCode 1427 Java solve by Henry

--- src/main/java/no1427/Solution.java
+++ src/main/java/no1427/Solution.java
@@ -5,5 +5,18 @@
 
 package no1427;
 
-public class Solution {
+class Solution {
+    public String stringShift(String s, int[][] shift) {
+        int len = s.length();
+        for (int[] array : shift) {
+            int a = array[0];
+            int b = array[1] % len;
+            if (a == 0) {
+                s = s.substring(b) + s.substring(0, b);
+            } else {
+                s = s.substring(len - b) + s.substring(0, len - b);
+            }
+        }
+        return s;
+    }
 }

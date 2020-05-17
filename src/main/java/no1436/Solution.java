586756ce2c242e7707c52beae6849d7fd8bbc2f8
henryxiao <fortbox1988@hotmail.com>
2020-05-17 15:35:10 +0800

    LeetCode 1436 Java solve by Henry

--- src/main/java/no1436/Solution.java
+++ src/main/java/no1436/Solution.java
@@ -5,5 +5,23 @@
 
 package no1436;
 
-public class Solution {
+import java.util.List;
+
+class Solution {
+    public String destCity(List<List<String>> paths) {
+        for (List<String> path : paths) {
+            String s = path.get(path.size() - 1);
+            if (isEnd(s, paths)) return s;
+        }
+        return null;
+    }
+
+    private boolean isEnd(String s, List<List<String>> paths) {
+        for (List<String> path : paths) {
+            if (path.contains(s) && !path.get(path.size() - 1).equals(s)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }

6786d6fa0e1e11f56b5d8a99601ae3d936b2e808
henryxiao <fortbox1988@hotmail.com>
2020-05-17 14:18:16 +0800

    LeetCode 1408 Java solve by Henry

--- src/main/java/no1408/Solution.java
+++ src/main/java/no1408/Solution.java
@@ -5,5 +5,19 @@
 
 package no1408;
 
-public class Solution {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public List<String> stringMatching(String[] words) {
+//        Arrays.sort(words,(Comparator.comparingInt(String::length)));
+        List<String> list = new ArrayList<>();
+        for (String word : words) {
+            if (Arrays.stream(words).filter(s -> (!word.equals(s) && s.contains(word))).count() > 0) list.add(word);
+        }
+        return list;
+
+
+    }
 }

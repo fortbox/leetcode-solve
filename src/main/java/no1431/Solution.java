48745166c548c571bee69ce43443248c819e9dae
henryxiao <fortbox1988@hotmail.com>
2020-05-17 15:24:44 +0800

    LeetCode 1431 Java solve by Henry

--- src/main/java/no1431/Solution.java
+++ src/main/java/no1431/Solution.java
@@ -5,5 +5,23 @@
 
 package no1431;
 
-public class Solution {
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int maxV = findMax(candies);
+        List<Boolean> list = new ArrayList<>();
+        for (int candy : candies) list.add(candy + extraCandies >= maxV);
+        return list;
+    }
+
+    private int findMax(int[] candies) {
+        int res = Integer.MIN_VALUE;
+        for (int candy : candies) {
+            if (candy > res) res = candy;
+        }
+        return res;
+    }
 }
+

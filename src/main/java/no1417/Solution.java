5992f0f8ab62b25eedb8bff3443b1b908b416061
henryxiao <fortbox1988@hotmail.com>
2020-05-17 14:43:16 +0800

    LeetCode 1417 Java solve by Henry

--- src/main/java/no1417/Solution.java
+++ src/main/java/no1417/Solution.java
@@ -5,5 +5,42 @@
 
 package no1417;
 
-public class Solution {
+import java.util.ArrayList;
+
+class Solution {
+    public String reformat(String s) {
+        ArrayList<Character> list0 = new ArrayList<>();
+        ArrayList<Character> list1 = new ArrayList<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) list0.add(c);
+            if (Character.isLetter(c)) list1.add(c);
+        }
+        if (Math.abs(list0.size() - list1.size()) > 1) return "";
+        int i = 0, j = 0;
+        boolean f = list0.size() > list1.size();
+        StringBuilder builder = new StringBuilder();
+        while (i < list0.size() || j < list1.size()) {
+            if (f) {
+                if (i < list0.size()) {
+                    builder.append(list0.get(i));
+                    i++;
+                }
+                if (j < list1.size()) {
+                    builder.append(list1.get(j));
+                    j++;
+                }
+            } else {
+                if (j < list1.size()) {
+                    builder.append(list1.get(j));
+                    j++;
+                }
+                if (i < list0.size()) {
+                    builder.append(list0.get(i));
+                    i++;
+                }
+            }
+        }
+        return builder.toString();
+    }
 }

8abbd168808aa2a052289ccdb701dec76f65c28d
henryxiao <fortbox1988@hotmail.com>
2020-05-17 14:53:13 +0800

    LeetCode 1422 Java solve by Henry

--- src/main/java/no1422/Solution.java
+++ src/main/java/no1422/Solution.java
@@ -5,5 +5,27 @@
 
 package no1422;
 
-public class Solution {
+class Solution {
+    public int maxScore(String s) {
+        int res = Integer.MIN_VALUE;
+        for (int i = 1; i < s.length(); i++) {
+            String t1 = s.substring(0, i);
+            String t2 = s.substring(i);
+            int ans = sum(t1, t2);
+            if (ans > res) res = ans;
+        }
+        return res;
+
+    }
+
+    private int sum(String t1, String t2) {
+        int n = 0;
+        for (char c : t1.toCharArray()) {
+            if (c == '0') n++;
+        }
+        for (char c : t2.toCharArray()) {
+            if (c == '1') n++;
+        }
+        return n;
+    }
 }

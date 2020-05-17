36b3f5fbb4c03f269313516d2ec49e973e035eee
henryxiao <fortbox1988@hotmail.com>
2020-05-17 14:30:19 +0800

    LeetCode 1413 Java solve by Henry

--- src/main/java/no1413/Solution.java
+++ src/main/java/no1413/Solution.java
@@ -1,9 +1,21 @@
+package no1413;
+
 /*
  * Copyright (c) 2020
  * Author: xiaoweixiang
  */
-
-package no1413;
-
-public class Solution {
+class Solution {
+    public int minStartValue(int[] nums) {
+        int ans = 1;
+        int pre = 1;
+        for (int i = 0; i < nums.length; i++) {
+            if (pre + nums[i] < 1) {
+                ans += (1 - pre - nums[i]);
+                pre = 1;
+            } else {
+                pre += nums[i];
+            }
+        }
+        return ans;
+    }
 }

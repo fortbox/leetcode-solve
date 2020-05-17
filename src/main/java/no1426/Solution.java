4ae42d69b8676b2e1c45e3a74949b3636004ea74
henryxiao <fortbox1988@hotmail.com>
2020-05-17 15:09:52 +0800

    LeetCode 1426 Java solve by Henry

--- src/main/java/no1426/Solution.java
+++ src/main/java/no1426/Solution.java
@@ -5,5 +5,17 @@
 
 package no1426;
 
-public class Solution {
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class Solution {
+    public int countElements(int[] arr) {
+        List<Integer> list = Arrays.stream(arr).boxed().collect(Collectors.toList());
+        int res = 0;
+        for (int i : arr) {
+            if (list.contains(i + 1)) res++;
+        }
+        return res;
+    }
 }

a6f91f1355a0bc39c2ec4cbc9f67b5e5a20ea39a
henryxiao <fortbox1988@hotmail.com>
2020-05-17 14:07:16 +0800

    LeetCode 1403 Java solve by Henry

--- src/main/java/no1403/Solution.java
+++ src/main/java/no1403/Solution.java
@@ -5,5 +5,23 @@
 
 package no1403;
 
-public class Solution {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public List<Integer> minSubsequence(int[] nums) {
+        Arrays.sort(nums);
+        int sum = Arrays.stream(nums).reduce(0, (a, b) -> a + b);
+        int last = 0;
+        int first = sum;
+        List<Integer> list = new ArrayList<>();
+        for (int i = nums.length - 1; i >= 0; i--) {
+            list.add(nums[i]);
+            last += nums[i];
+            first -= nums[i];
+            if (last > first) break;
+        }
+        return list;
+    }
 }

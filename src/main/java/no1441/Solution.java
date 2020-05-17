89cb0e97de4bbadf2dedeef93ceaa445137c69bb
henryxiao <fortbox1988@hotmail.com>
2020-05-17 15:54:20 +0800

    LeetCode 1441 Java solve by Henry

---
+++ src/main/java/no1441/Solution.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2020
+ * Author: xiaoweixiang
+ */
+
+package no1441;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    private static final String PUSH = "Push";
+    private static final String POP = "Pop";
+
+    public List<String> buildArray(int[] target, int n) {
+        int m = 1;
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < target.length; i++) {
+            int k = target[i];
+            while (true) {
+                if (k == m) {
+                    m++;
+                    list.add(PUSH);
+                    break;
+                } else {
+                    m++;
+                    list.add(PUSH);
+                    list.add(POP);
+                }
+            }
+
+        }
+        return list;
+    }
+}

a8bf4e8c48d6ca9e54c5b9f251288a74a4f85fc2
henryxiao <fortbox1988@hotmail.com>
2020-07-25 20:30:13 +0800

    2020-07-25

---
+++ src/main/java/no1443/Solution.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2020
+ * Author: xiaoweixiang
+ */
+
+package no1443;
+
+import java.util.List;
+
+class Solution {
+    public int minTime(int n, int[][] edges, List<Boolean> hasApple) {
+        // 一点思路也没有，
+        // 参考了其他人的解题
+        // 利用了题目默认条件
+        int res = 0;
+        for (int i = edges.length - 1; i >= 0; i--) if (hasApple.get(edges[i][1])) hasApple.set(edges[i][0], true);
+        for (int[] edge : edges) if (hasApple.get(edge[1])) res += 2;
+        return res;
+
+    }
+}
---
+++ src/main/java/no1457/Solution.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2020
+ * Author: xiaoweixiang
+ */
+
+package no1457;
+
+import tool.TreeNode;
+
+import java.util.*;
+
+class Solution {
+    private final List<String> pathList = new ArrayList<String>();
+
+    public int pseudoPalindromicPaths(TreeNode root) {
+        /**
+         * 1，获取所有路径
+         * 2，判断回文
+         */
+        int ans = 0;
+        dfs(root, "");
+        for (String s : pathList) {
+            String[] array = s.split("|");
+            System.out.println("Arrays.toString(array) = " + Arrays.toString(array));
+            HashMap<String, Integer> map = new HashMap<>();
+            for (String s1 : array) {
+                map.put(s1, map.getOrDefault(s1, 0) + 1);
+            }
+            int count = 0;
+            for (Map.Entry<String, Integer> entry : map.entrySet()) {
+                if (!entry.getKey().equals("|")) if (entry.getValue() % 2 == 1) count++;
+            }
+            if (count <= 1) ans++;
+        }
+        return ans;
+    }
+
+    private void dfs(TreeNode root, String s) {
+        if (root == null) return;
+        if (root.left == null && root.right == null) {
+            System.out.println("s = " + s);
+            pathList.add(s + "|" + root.val);
+            return;
+        }
+        if (root.left != null) dfs(root.left, s + "|" + root.val);
+        if (root.right != null) dfs(root.right, s + "|" + root.val);
+    }
+}

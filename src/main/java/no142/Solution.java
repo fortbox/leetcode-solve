3b6df4f265b9558483d7ab15bab3c94ab728563c
henryxiao <fortbox1988@outlook.com>
2020-08-25 14:04:43 +0800

    2020-08-25  leetcode  solve java by henry

---
+++ src/main/java/no142/Solution.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright(c) 2020-2020
+ * Author: xiaoweixiang
+ */
+
+package no142;
+
+import java.util.HashSet;
+
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        HashSet<ListNode> set = new HashSet<>();
+        while (head != null) {
+            if (set.contains(head)) return head;
+            set.add(head);
+            head = head.next;
+        }
+        return null;
+    }
+
+    // Definition for singly-linked list.
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
+
+
---
+++ src/main/java/no151/Solution.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright(c) 2020-2020
+ * Author: xiaoweixiang
+ */
+
+package no151;
+
+import java.util.Arrays;
+
+class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String s = "  hello world!  ";
+        String ans = solution.reverseWords(s);
+        System.out.println("ans = " + ans);
+    }
+
+    public String reverseWords(String s) {
+        s = s.trim();
+        // 正则表达式
+        String[] array = s.split("\\s+");
+        System.out.println("Arrays.toString(array) = " + Arrays.toString(array));
+        StringBuilder builder = new StringBuilder();
+        for (int i = array.length - 1; i >= 0; i--) {
+            if (array[i] != null && !"".equals(array[i])) {
+                builder.append(array[i]);
+                if (i > 0) builder.append(" ");
+            }
+        }
+        return builder.toString();
+    }
+}

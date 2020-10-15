cd4d72ad48cfcc06fda400d18b41b98ed55caba6
henryxiao <fortbox1988@hotmail.com>
2020-10-16 01:16:54 +0800

    1231，2020-10-06

---
+++ src/main/java/no1231/Solution.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2020
+ * Author: xiaoweixiang
+ */
+
+package no1231;
+
+class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] sweetness = {1, 2, 3, 4, 5, 6, 7, 8,};
+        int k = 5;
+        int res = solution.maximizeSweetness(sweetness, k);
+        System.out.println("res = " + res);
+    }
+
+    public int maximizeSweetness(int[] sweetness, int K) {
+
+    }
+}

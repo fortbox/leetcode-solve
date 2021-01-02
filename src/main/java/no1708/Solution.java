c3e74837bea08842f04a327fb6fee60b793bf25c
henryxiao <fortbox1988@hotmail.com>
2021-01-02 16:18:12 +0800

    1708，2021-01-02

---
+++ src/main/java/no1708/Solution.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2021
+ * Author: xiaoweixiang
+ */
+
+package no1708;
+
+import java.util.Arrays;
+
+class Solution {
+    public int[] largestSubarray(int[] nums, int k) {
+        int length = nums.length;
+        int maxValueIndex = 0;
+        for (int i = 0; i < nums.length - k + 1; i++) {
+            if (nums[i] > nums[maxValueIndex]) {
+                maxValueIndex = i;
+            }
+        }
+        return Arrays.copyOfRange(nums, maxValueIndex, maxValueIndex + k);
+    }
+}

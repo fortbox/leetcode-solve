98d7f17381293946627f44ca5320d3ac7ab896bf
henryxiao <fortbox1988@hotmail.com>
2021-01-28 09:06:20 +0800

    1032

---
+++ src/main/java/no1698/Solution.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2021
+ * Author: xiaoweixiang
+ */
+
+package no1698;
+
+import java.util.HashSet;
+
+class Solution {
+    public int countDistinct(String s) {
+        HashSet<String> set = new HashSet<>();
+        int length = s.length();
+        for (int i = 0; i < length; i++) {
+            for (int j = length; j > i; j--) {
+                String s1 = s.substring(i, j);
+                set.add(s1);
+            }
+        }
+        return set.size();
+    }
+}

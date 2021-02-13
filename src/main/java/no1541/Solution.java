3c18c7451b0d41405b2f52ff70ab103769742ffd
henryxiao <fortbox1988@hotmail.com>
2021-02-13 16:39:00 +0800

    1541

---
+++ src/main/java/no1541/Solution.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2021
+ * Author: xiaoweixiang
+ */
+
+package no1541;
+
+class Solution {
+    public int minInsertions(String s) {
+        char[] chars = s.toCharArray();
+        int res = 0;
+        int len = s.length();
+        int left = 0;
+        for (int i = 0; i < len; i++) {
+            if (chars[i] == '(') {
+                left++;
+            } else {
+                if (left == 0) res++;
+                else left--;
+                if (i == len - 1 || chars[i + 1] != ')') res++;
+                else i++;
+            }
+        }
+        return res + left * 2;
+    }
+}
